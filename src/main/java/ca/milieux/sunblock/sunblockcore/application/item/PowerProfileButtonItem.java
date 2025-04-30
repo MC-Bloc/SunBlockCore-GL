fc697cb1d85a5bbc1090dff9dc46b23103f972c5
Shahrom <mshahromali@gmail.com>
2025-04-30 19:22:48 -0400

    Create PowerProfileButtonItem.java

---
+++ src/main/java/ca/milieux/sunblock/sunblockcore/application/item/PowerProfileButtonItem.java
@@ -0,0 +1,16 @@
+package ca.milieux.sunblock.sunblockcore.application.item;
+
+import net.minecraft.world.level.block.Block;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class PowerProfileButtonItem extends Block {
+
+    public PowerProfileButtonItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+}
