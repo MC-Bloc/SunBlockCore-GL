75648eb47a028b12575b1836d26ee48bee247b9e
Shahrom <mshahromali@gmail.com>
2025-04-30 19:23:35 -0400

    Update PowerProfileButtonItem.java

--- src/main/java/ca/milieux/sunblock/sunblockcore/application/item/PowerProfileButtonItem.java
+++ src/main/java/ca/milieux/sunblock/sunblockcore/application/item/PowerProfileButtonItem.java
@@ -13,4 +13,24 @@ public class PowerProfileButtonItem extends Block {
         super(pProperties);
     }
 
+    public static void PerformanceMode(){
+        try {
+            URL switch_performance = new URL("");
+            URLConnection yc = switch_performance.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        } catch (Exception e) {
+            System.err.println("SunBlockCore::DataQueryProcess -- PerformanceMode() Error " + e.getMessage());
+        }
+    }
+
+    public static void PowerSaverMode(){
+        try {
+            URL switch_powersaver = new URL("");
+            URLConnection yc = switch_powersaver.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+        } catch (Exception e) {
+            System.err.println("SunBlockCore::DataQueryProcess -- PowerSaverMode() Error " + e.getMessage());
+        }
+    }
 }
