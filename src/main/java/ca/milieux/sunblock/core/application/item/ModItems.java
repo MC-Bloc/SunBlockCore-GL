586806fc24ecbe9ff4625fe25830072318127e68
Shahrom Ali <mshahromali@gmail.com>
2026-03-07 01:28:54 -0500

    Update ModItems.java
    formatting only

--- src/main/java/ca/milieux/sunblock/core/application/item/ModItems.java
+++ src/main/java/ca/milieux/sunblock/core/application/item/ModItems.java
@@ -14,19 +14,39 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, SunBlockCore.MODID);
 
     public static final RegistryObject<Item> SOLAR_SWORD = ITEMS.register("solar_sword",
-            () -> new SolarSword(Tiers.IRON, 3, -2.4f, new Item.Properties()));
+            () -> new SolarSword(
+                    Tiers.IRON,
+                    3,
+                    -2.4f,
+                    new Item.Properties()));
 
     public static final RegistryObject<Item> SOLAR_PICKAXE = ITEMS.register("solar_pickaxe",
-            () -> new SolarPickaxe(Tiers.IRON, 3, -2.4f, new Item.Properties()));
+            () -> new SolarPickaxe(
+                    Tiers.IRON,
+                    3,
+                    -2.4f,
+                    new Item.Properties()));
 
     public static final RegistryObject<Item> SOLAR_AXE = ITEMS.register("solar_axe",
-            () -> new SolarAxe(Tiers.IRON, 3, -2.4f, new Item.Properties()));
+            () -> new SolarAxe(
+                    Tiers.IRON,
+                    3,
+                    -2.4f,
+                    new Item.Properties()));
 
     public static final RegistryObject<Item> SOLAR_HOE = ITEMS.register("solar_hoe",
-            () -> new SolarHoe(Tiers.IRON, 3, -2.4f, new Item.Properties()));
+            () -> new SolarHoe(
+                    Tiers.IRON,
+                    3,
+                    -2.4f,
+                    new Item.Properties()));
 
     public static final RegistryObject<Item> SOLAR_SHOVEL = ITEMS.register("solar_shovel",
-            () -> new SolarShovel(Tiers.IRON, 3, -2.4f, new Item.Properties()));
+            () -> new SolarShovel(
+                    Tiers.IRON,
+                    3,
+                    -2.4f,
+                    new Item.Properties()));
 
     public static void register(IEventBus eventBus) {ITEMS.register(eventBus);}
 
