334a65b242109ee091f9a77f346a3b0e7f4cf2b2
Shahrom Ali <mshahromali@gmail.com>
2026-03-07 01:29:06 -0500

    Update ClientEventHandler.java
    Pickaxe and shovel speed

--- src/main/java/ca/milieux/sunblock/core/application/client/ClientEventHandler.java
+++ src/main/java/ca/milieux/sunblock/core/application/client/ClientEventHandler.java
@@ -1,8 +1,11 @@
 package ca.milieux.sunblock.core.application.client;
 
+import ca.milieux.sunblock.core.services.DataQueryProcess;
+import ca.milieux.sunblock.core.services.SolarDataTypes;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 
@@ -18,6 +21,15 @@ public class ClientEventHandler {
 			HUD.TextHUDDetailed(event);
 		} else if (HUD.type == HUDType.GraphicalV0) {
 			HUD.GraphicHUD(event);
+		}
+	}
+
+	@SubscribeEvent
+	public void onBlockStartBreak(PlayerEvent.BreakSpeed event){
+		String item = event.getEntity().getMainHandItem().toString();
+		float inc_speed = Math.max(DataQueryProcess.GetServerData(SolarDataTypes.PVPOWER), event.getOriginalSpeed());
+		if (item.contains("solar_pickaxe") || item.contains("solar_shovel")){
+			event.setNewSpeed(inc_speed);
 		}
 	}
 }
