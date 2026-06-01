2be78b914479c5b8650d4505940e160f799738f4
Shahrom Ali <estineali@gmail.com>
2026-05-31 20:43:20 -0400

    resource loading optimizations

--- src/main/java/ca/milieux/sunblock/core/application/client/HUD.java
+++ src/main/java/ca/milieux/sunblock/core/application/client/HUD.java
@@ -21,6 +21,15 @@ public class HUD {
     public static int statsIndex = 0;
     public static HUDType type = HUDType.GraphicalV0;
 
+    static ResourceLocation BG_DAWN = null, BG_DAY = null, BG_DUSK = null, BG_NIGHT = null;
+    static ResourceLocation CPU_HIGH = null, CPU_MED = null, CPU_LOW = null;
+    static ResourceLocation LOAD_HIGH = null, LOAD_MED = null, LOAD_LOW = null;
+    static ResourceLocation GEN_HIGH = null, GEN_MED = null, GEN_LOW = null;
+    static ResourceLocation BATT_HIGH = null, BATT_MED = null, BATT_LOW = null, BATT_DAM = null;
+    static ResourceLocation TIME_HIGH = null, TIME_MED = null, TIME_LOW = null;
+    static ResourceLocation BATT_CHARGING = null, BATT_DISCHARGE = null;
+
+
     public static void TextHUD(RenderGuiOverlayEvent.Post event) {
         if (mc.player != null && mc.level != null && !mc.options.hideGui && (mc.screen == null || (ConfigHandler.CLIENT.displayWithChatOpen.get() && mc.screen instanceof ChatScreen))) {
 
@@ -88,14 +97,11 @@ public class HUD {
 
             String timeString = SolarServerData.getTimestamp();
 
-
-
             double configScale = ConfigHandler.CLIENT.HUD_SCALE.get();
             double configOpacity = ConfigHandler.CLIENT.HUD_OPACITY.get();
             float op = (float) configOpacity / 10;
 
             int textColor = 0xDDDDDD;
-            String texturePath = GetTexturePath(timeString);
 
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
@@ -106,20 +112,19 @@ public class HUD {
     		guiGraphics.pose().scale((float)configScale, (float)configScale, 1.0F);
 
 
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, texturePath));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, texturePath),
-                    0, 0, 0, 0, 168, 126, 168, 126);
+            //
+            ResourceLocation bg_texture = GetTexture(timeString);
+            RenderSystem.setShaderTexture(0, bg_texture);
+            guiGraphics.blit(bg_texture,0, 0, 0, 0, 168, 126, 168, 126);
 
             RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
 
             // Draw text with modified alpha scaling.
             guiGraphics.drawString(mc.font, "Montreal, QC " + timeString, 60, 19, textColor);
 
-            int _px = 25;
-            //There is a difference of 4 pixels between the line and the graphic.
-            int diff = 4;
 
-            int _py_line1 = 40;
+            int _px = 25, _py_line1 = 40, diff = 4; // There is a difference of 4 pixels between the line and the graphic.
+            String delim = "\n\n";
 
 
             String solar_stats = "SOLAR: " + SolarServerData.getPvVoltage() + "v | " + SolarServerData.getPvPower() + "w";
@@ -130,155 +135,151 @@ public class HUD {
                     "TIME LEFT: " + SolarServerData.getTimeRemaining() + " Hours";
             String power_profile = "POWER MODE:" + SolarServerData.getPowerProfile();
 
-            String delim = "\n\n";
 
             FormattedText t = FormattedText.of(solar_stats + delim + battery_stats + delim + usage_stats + delim + time_left + delim + power_profile);
             guiGraphics.drawWordWrap(mc.font, t, _px, _py_line1,168, textColor);
 
-
-//            guiGraphics.drawString(mc.font, "SOLAR: " + SolarServerData.getPvVoltage() + "v | " + SolarServerData.getPvPower() + "w", _px, _py_line1, textColor);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetGenerationIcon()));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetGenerationIcon()), 3, _py_line1 - diff, 0, 0, 16, 16, 16, 16);
+            ResourceLocation genIcon = GetGenerationIcon();
+            RenderSystem.setShaderTexture(0, genIcon);
+            guiGraphics.blit(genIcon, 3, _py_line1 - diff, 0, 0, 16, 16, 16, 16);
 
             int _py_line2 = 58;
-//            guiGraphics.drawString(mc.font, "BATTERY: " + SolarServerData.getBattVoltage() + "v | " + SolarServerData.getBattRemaining() + "%", _px, _py_line2, textColor);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetBatteryIcon()));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetBatteryIcon()), 3, _py_line2 - diff, 0, 0, 16, 16, 16, 16);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetBatteryArrowIcon(SolarServerData.getBattOverallCurrent())));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetBatteryArrowIcon(SolarServerData.getBattOverallCurrent())),
-                    12, _py_line2 - diff, 0, 0, 16, 16, 16, 16);
+            ResourceLocation batt_icon = GetBatteryIcon(), batt_arrow = GetBatteryArrowIcon();
+            RenderSystem.setShaderTexture(0, batt_icon);
+            guiGraphics.blit(batt_icon, 3, _py_line2 - diff, 0, 0, 16, 16, 16, 16);
+            RenderSystem.setShaderTexture(0, batt_arrow);
+            guiGraphics.blit(batt_arrow, 12, _py_line2 - diff, 0, 0, 16, 16, 16, 16);
 
             int _py_line3 = 76;
-//            guiGraphics.drawString(mc.font, "USAGE: " + SolarServerData.getlPower() + "w", _px, _py_line3, textColor);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetLoadIcon()));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetLoadIcon()), 3, _py_line3 - diff, 0, 0, 16, 16, 16, 16);
+            ResourceLocation load_icon = GetLoadIcon();
+            RenderSystem.setShaderTexture(0, load_icon);
+            guiGraphics.blit(load_icon, 3, _py_line3 - diff, 0, 0, 16, 16, 16, 16);
 
             int _py_line4 = 94;
-//            guiGraphics.drawString(mc.font, "TIME LEFT: " + SolarServerData.getTimeRemaining() + " Hours", _px, _py_line4, textColor);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetTimeRemainingIcon()));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetTimeRemainingIcon()), 3, _py_line4 - 4, 0, 0, 16, 16, 16, 16);
+            ResourceLocation time_icon = GetTimeRemainingIcon();
+            RenderSystem.setShaderTexture(0, time_icon);
+            guiGraphics.blit(time_icon, 3, _py_line4 - 4, 0, 0, 16, 16, 16, 16);
 
             int _py_line5 = 112;
-//            guiGraphics.drawString(mc.font, "POWER MODE:" + SolarServerData.getPowerProfile(), _px, _py_line5, textColor);
-            RenderSystem.setShaderTexture(0, new ResourceLocation(SunBlockCore.MODID, GetCPUIcon()));
-            guiGraphics.blit(new ResourceLocation(SunBlockCore.MODID, GetCPUIcon()), 3, _py_line5 - 4, 0, 0, 16, 16, 16, 16);
+            ResourceLocation cpu_icon = GetCPUIcon();
+            RenderSystem.setShaderTexture(0, cpu_icon);
+            guiGraphics.blit(cpu_icon, 3, _py_line5 - 4, 0, 0, 16, 16, 16, 16);
 
 
             guiGraphics.pose().popPose();
-
-            // Reset shader color.
-            RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+            RenderSystem.setShaderColor(1f, 1f, 1f, 1f); // Reset shader color.
         }
     }
 
 
+    static ResourceLocation GetTexture(String timestamp) {
+        int DAWN = 6, DAY = 9, EVENING = 18, NIGHT = 21;
 
-    static String GetTexturePath(String timestamp) {
-        int DAWN = 6;
-        int DAY = 9;
-        int EVENING = 18;
-        int NIGHT = 21;
+        // init on first use
+        if (BG_DAWN == null) BG_DAWN = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_hud_dawn.png" );
+        if (BG_DAY == null) BG_DAY = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_hud_day.png" );
+        if (BG_DUSK == null) BG_DUSK = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_hud_dusk.png" );
+        if (BG_NIGHT == null) BG_NIGHT = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_hud_night.png" );
 
-        if (timestamp == null || timestamp.isEmpty() || timestamp == "") {
-            return "textures/gui/mc_sb_hud_day.png";
+        // base case
+        if (timestamp == null || timestamp.isEmpty()) {
+            return BG_DAY;
         }
 
         try {
             int hour = Integer.parseInt(timestamp.split(":")[0]);
-            if (hour >= DAWN && hour < DAY) {
-                return "textures/gui/mc_sb_hud_dawn.png";
-            } else if (hour >= DAY && hour < EVENING) {
-                return "textures/gui/mc_sb_hud_day.png";
-            } else if (hour >= EVENING && hour < NIGHT) {
-                return "textures/gui/mc_sb_hud_dusk.png";
-            } else {
-                return "textures/gui/mc_sb_hud_night.png";
-            }
+            if (hour >= NIGHT) return BG_NIGHT;
+            else if (hour >= EVENING) return BG_DUSK;
+            else if (hour >= DAY) return BG_DAY;
+            else return BG_DAWN;
         } catch (Exception e) {
             LOGGER.error("Error parsing timestamp '{}': {}", timestamp, e.getMessage());
-            return "textures/gui/mc_sb_hud_day.png";
+            return BG_DAY;
         }
     }
 
-    static String GetCPUIcon() {
-        int LOW_THRESH_CPUPOWER = 5;
-        int HIGH_THRESH_CPUPOWER = 20;
+    static ResourceLocation GetCPUIcon() {
+        int LOW_THRESH_CPUPOWER = 5, HIGH_THRESH_CPUPOWER = 20;
         float cpuPowerDraw = SolarServerData.getPower();
 
-        if (cpuPowerDraw < LOW_THRESH_CPUPOWER) {
-            return "textures/gui/mc_sb_icons_iso_cpu_g.png";
-        } else if (cpuPowerDraw < HIGH_THRESH_CPUPOWER) {
-            return "textures/gui/mc_sb_icons_iso_cpu_y.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_cpu_r.png";
-        }
+        // init on first use
+        if (CPU_LOW == null) CPU_LOW = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_cpu_g.png");
+        if (CPU_MED == null) CPU_MED = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_cpu_y.png");
+        if (CPU_HIGH == null) CPU_HIGH = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_cpu_r.png");
+
+        if (cpuPowerDraw < LOW_THRESH_CPUPOWER) return CPU_LOW;
+        else if (cpuPowerDraw < HIGH_THRESH_CPUPOWER) return CPU_MED;
+        else return CPU_HIGH;
     }
 
-    static String GetLoadIcon() {
+    static ResourceLocation GetLoadIcon() {
         int MIN_THRESH_LPOWER = 10;
         int MAX_THRESH_LPOWER = 22;
         float loadPower = SolarServerData.getlPower();
 
-        if (loadPower < MIN_THRESH_LPOWER) {
-            return "textures/gui/mc_sb_icons_iso_globe_g.png";
-        } else if (loadPower < MAX_THRESH_LPOWER) {
-            return "textures/gui/mc_sb_icons_iso_globe_y.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_globe_r.png";
-        }
+        // init on first use
+        if (LOAD_LOW == null) LOAD_LOW = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_globe_g.png");
+        if (LOAD_MED == null) LOAD_MED = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_globe_y.png");
+        if (LOAD_HIGH == null) LOAD_HIGH = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_globe_r.png");
+
+        if (loadPower < MIN_THRESH_LPOWER) return LOAD_LOW;
+        else if (loadPower < MAX_THRESH_LPOWER) return LOAD_MED;
+        else return LOAD_HIGH;
     }
 
-    static String GetGenerationIcon() {
+    static ResourceLocation GetGenerationIcon() {
         int MIN_THRESH_PVPOWER = 10;
         int MAX_THRESH_PVPOWER = 22;
         float pvPower = SolarServerData.getPvPower();
 
-        if (pvPower < MIN_THRESH_PVPOWER) {
-            return "textures/gui/mc_sb_icons_iso_sun_r.png";
-        } else if (pvPower < MAX_THRESH_PVPOWER) {
-            return "textures/gui/mc_sb_icons_iso_sun_o.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_sun.png";
-        }
+        // init on first use
+        if (GEN_LOW == null) GEN_LOW = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_sun_r.png");
+        if (GEN_MED == null) GEN_MED = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_sun_o.png");
+        if (GEN_HIGH == null) GEN_HIGH = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_sun.png");
+
+        if (pvPower < MIN_THRESH_PVPOWER) return GEN_LOW;
+        else if (pvPower < MAX_THRESH_PVPOWER) return GEN_MED;
+        else return GEN_HIGH;
     }
 
-    static String GetBatteryIcon() {
+    static ResourceLocation GetBatteryIcon() {
         int HIGH_THRESH_BATTERY = 75;
         int MED_THRESH_BATTERY = 50;
         int LOW_THRESH_BATTERY = 25;
         float battPercentage = SolarServerData.getBattRemaining();
 
-        if (battPercentage > HIGH_THRESH_BATTERY) {
-            return "textures/gui/mc_sb_icons_iso_battery_g.png";
-        } else if (battPercentage > MED_THRESH_BATTERY) {
-            return "textures/gui/mc_sb_icons_iso_battery_y.png";
-        } else if (battPercentage > LOW_THRESH_BATTERY) {
-            return "textures/gui/mc_sb_icons_iso_battery_o.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_battery_r.png";
-        }
+        // init on first use
+        if (BATT_DAM == null) BATT_DAM = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_battery_r.png");
+        if (BATT_LOW == null) BATT_LOW = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_battery_o.png");
+        if (BATT_MED == null) BATT_MED = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_battery_y.png");
+        if (BATT_HIGH == null) BATT_HIGH = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_battery_g.png");
+
+        if (battPercentage > HIGH_THRESH_BATTERY) return BATT_HIGH;
+        else if (battPercentage > MED_THRESH_BATTERY) return BATT_MED;
+        else if (battPercentage > LOW_THRESH_BATTERY) return BATT_LOW;
+        else return BATT_DAM;
     }
 
-    static String GetTimeRemainingIcon() {
+    static ResourceLocation GetTimeRemainingIcon() {
         int HIGH_THRESH_BATTERY = 40;
         int MED_THRESH_BATTERY = 12;
         float hours_rem = Float.parseFloat(SolarServerData.getTimeRemaining());
 
-        if (hours_rem > HIGH_THRESH_BATTERY) {
-            return "textures/gui/mc_sb_icons_iso_clock_g.png";
-        } else if (hours_rem > MED_THRESH_BATTERY) {
-            return "textures/gui/mc_sb_icons_iso_clock_y.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_clock_r.png";
-        }
+        if (TIME_LOW == null) TIME_LOW = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_clock_r.png");
+        if (TIME_MED == null) TIME_MED = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_clock_y.png");
+        if (TIME_HIGH == null) TIME_HIGH = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_clock_g.png");
+
+        if (hours_rem > HIGH_THRESH_BATTERY) return TIME_HIGH;
+        else if (hours_rem > MED_THRESH_BATTERY) return TIME_MED;
+        else return TIME_LOW;
     }
 
-    static String GetBatteryArrowIcon(float battOverallCurrent) {
-        if (battOverallCurrent > 0) {
-            return "textures/gui/mc_sb_icons_iso_arrow_up.png";
-        } else {
-            return "textures/gui/mc_sb_icons_iso_arrow_down.png";
-        }
+    static ResourceLocation GetBatteryArrowIcon() {
+        if (BATT_CHARGING == null) BATT_CHARGING = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_arrow_up.png");
+        if (BATT_DISCHARGE == null) BATT_DISCHARGE = new ResourceLocation(SunBlockCore.MODID, "textures/gui/mc_sb_icons_iso_arrow_down.png");
+
+        if (SolarServerData.getBattOverallCurrent() > 0) return BATT_CHARGING;
+        else return BATT_DISCHARGE;
     }
 
 }
