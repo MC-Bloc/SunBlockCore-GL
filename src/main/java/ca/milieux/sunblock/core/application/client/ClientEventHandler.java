2f211d86a78ca78d76877903e5d464d9bc5a84e5
Shahrom Ali <estineali@gmail.com>
2026-06-20 22:25:49 -0400

    onAddOverlayLayers

--- src/main/java/ca/milieux/sunblock/core/application/client/ClientEventHandler.java
+++ src/main/java/ca/milieux/sunblock/core/application/client/ClientEventHandler.java
@@ -1,24 +1,29 @@
 package ca.milieux.sunblock.core.application.client;
 
+import ca.milieux.sunblock.core.SunBlockCore;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.event.AddGuiOverlayLayersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-
+import net.minecraftforge.fml.common.Mod;
 
 // All of this HUD rendering code is from Matt Czyr's Explorer's Compass. Thanks Matt! :)
-@OnlyIn(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = SunBlockCore.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventHandler {
 
-	@SubscribeEvent
-	public void onRenderTick(RenderGuiOverlayEvent.Post event) {
-		if (HUD.type == HUDType.TextV0) {
-			HUD.TextHUD(event);
-		} else if (HUD.type == HUDType.TextV1) {
-			HUD.TextHUDDetailed(event);
-		} else if (HUD.type == HUDType.GraphicalV0) {
-			HUD.GraphicHUD(event);
-		}
-	}
+    private static final ResourceLocation SUNBLOCK_HUD =
+            ResourceLocation.fromNamespaceAndPath(SunBlockCore.MODID, "hud");
 
+    @SubscribeEvent
+    public static void onAddOverlayLayers(AddGuiOverlayLayersEvent event) {
+        event.getLayeredDraw().add(SUNBLOCK_HUD, (guiGraphics, deltaTracker) -> {
+            if (HUD.type == HUDType.TextV0) {
+                HUD.TextHUD(guiGraphics);
+            } else if (HUD.type == HUDType.TextV1) {
+                HUD.TextHUDDetailed(guiGraphics);
+            } else if (HUD.type == HUDType.GraphicalV0) {
+                HUD.GraphicHUD(guiGraphics);
+            }
+        });
+    }
 }
